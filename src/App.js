53b29504c58f62badfb67c42ef1e50149cc07c49
BlakeVidebeck <blakevidebeck@gmail.com>
2020-10-27 19:55:50 +1300

    add components and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import CardList from './components/CardList';
+import SearchBox from './components/SearchBox';
+import Scroll from './components/Scroll';
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './app.css';
+
+const App = () => {
+	const [state, setState] = useState({
+		robots: [],
+		searchfield: ''
+	});
+
+	const { robots, searchfield } = state;
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users/')
+			.then(res => res.json())
+			.then(users => setState({ ...state, robots: users }));
+	}, [state]);
+
+	const onSearchChange = e => {
+		setState({ ...state, [e.target.name]: e.target.value });
+	};
+
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
+
+	return !robots.length ? (
+		<h1>LOADING</h1>
+	) : (
+		<div className='tc'>
+			<h1 className='f1'>RoboFriends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+		</div>
+	);
+};
 
 export default App;
